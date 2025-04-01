14c9061d50d434306c032fe8f00be833e76d1e4e
abdulqudoos9121 <aq31302890@gmail.com>
2025-04-02 01:43:41 +0500

    Java Program1

---
+++ Program1.java
@@ -0,0 +1,8 @@
+public class Program1{
+    public static int Sum(int x,int y){
+        return x+y;
+    }
+    public static void main(String []arg){
+        System.out.println(Sum(12,8));
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# java-practise-project
-simple project 
